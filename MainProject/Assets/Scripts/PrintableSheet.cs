78182adf4d4eb9b1b87eb21d6cf2008b3f1f4c55
ncik <nick@constantinidis.ca>
2014-06-20 23:39:03 -0400

    Card and print layouts

---
+++ MainProject/Assets/Scripts/PrintableSheet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintableSheet : MonoBehaviour
+{
+	public GameObject m_CardPrefab;
+	public GameObject m_Cards;
+	public Transform[] m_Anchors;
+
+	// Use this for initialization
+	void Start()
+	{
+		foreach (Transform anchor in m_Anchors)
+		{
+			GameObject card = Instantiate(m_CardPrefab, anchor.transform.position, Quaternion.identity) as GameObject;
+			card.transform.parent = m_Cards.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ MainProject/Assets/Scripts/RenderTexture.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class RenderTexture : MonoBehaviour
+{
+	public static string TEXTURE_PATH = "C:/Users/samir/Documents/GodsGame/Sheets/";
+	public static string TEXTURE_EXT = ".png";
+
+	public GameObject m_Paper;
+	public Rect m_Dimensions;
+	public string m_TextureName;
+
+	private int oldAntiAliasingSettings;
+	private Texture2D m_RenderTexture;
+	private int screenshotCounter = 2;
+
+	// Use this for initialization
+	void Start () {
+		oldAntiAliasingSettings = QualitySettings.antiAliasing;
+		QualitySettings.antiAliasing = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		--screenshotCounter;
+	}
+
+	void OnPostRender()
+	{
+		if (screenshotCounter <= 0)
+		{
+			m_RenderTexture = new Texture2D(Mathf.RoundToInt(Screen.width), Mathf.RoundToInt(Screen.height), TextureFormat.ARGB32, false);
+			m_RenderTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+			
+			byte[] bytes = m_RenderTexture.EncodeToPNG();
+			Destroy(m_RenderTexture);
+			
+			File.WriteAllBytes(TEXTURE_PATH + m_TextureName + TEXTURE_EXT, bytes);
+
+			QualitySettings.antiAliasing = oldAntiAliasingSettings;
+		}
+	}
+}
