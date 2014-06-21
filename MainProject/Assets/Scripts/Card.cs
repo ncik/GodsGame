415641936596dfd60a26268bbc49c3d3ad09c7f2
ncik <nick@constantinidis.ca>
2014-06-21 04:20:02 -0400

    Fixed hi-res texture printing

---
+++ MainProject/Assets/Scripts/Card.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour
+{
+	public TextMesh m_NameText;
+	public TextMesh m_PowerText;
+	public TextMesh m_AbilitiesText;
+
+	public GameObject m_UpArrow;
+	public GameObject m_DownArrow;
+	public GameObject m_LeftArrow;
+	public GameObject m_RightArrow;
+
+	public string m_CardName;
+	public int m_Power;
+	public string m_Abilities;
+
+	// Use this for initialization
+	void Start()
+	{
+		m_NameText.text = m_CardName;
+		m_PowerText.text = m_Power.ToString();
+		m_AbilitiesText.text = m_Abilities;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
--- MainProject/Assets/Scripts/RenderTexture.cs
+++ MainProject/Assets/Scripts/RenderTexture.cs
@@ -4,11 +4,11 @@ using System.IO;
 
 public class RenderTexture : MonoBehaviour
 {
-	public static string TEXTURE_PATH = "C:/Users/samir/Documents/GodsGame/Sheets/";
+	//public static string TEXTURE_PATH = "C:/Users/samir/Documents/GodsGame/Sheets/";
+	public static string TEXTURE_PATH = "C:/Users/Public/Documents/Unity Projects/GodsGame/Sheets/";
 	public static string TEXTURE_EXT = ".png";
 
 	public GameObject m_Paper;
-	public Rect m_Dimensions;
 	public string m_TextureName;
 
 	private int oldAntiAliasingSettings;
@@ -16,20 +16,21 @@ public class RenderTexture : MonoBehaviour
 	private int screenshotCounter = 2;
 
 	// Use this for initialization
-	void Start () {
-		oldAntiAliasingSettings = QualitySettings.antiAliasing;
-		QualitySettings.antiAliasing = 0;
+	void Start()
+	{
+		//oldAntiAliasingSettings = QualitySettings.antiAliasing;
+		//QualitySettings.antiAliasing = 0;
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
 	{
 		--screenshotCounter;
 	}
 
 	void OnPostRender()
 	{
-		if (screenshotCounter <= 0)
+		if (screenshotCounter == 0)
 		{
 			m_RenderTexture = new Texture2D(Mathf.RoundToInt(Screen.width), Mathf.RoundToInt(Screen.height), TextureFormat.ARGB32, false);
 			m_RenderTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
@@ -39,7 +40,9 @@ public class RenderTexture : MonoBehaviour
 			
 			File.WriteAllBytes(TEXTURE_PATH + m_TextureName + TEXTURE_EXT, bytes);
 
-			QualitySettings.antiAliasing = oldAntiAliasingSettings;
+			//QualitySettings.antiAliasing = oldAntiAliasingSettings;
+
+			Application.CaptureScreenshot(TEXTURE_PATH + m_TextureName + TEXTURE_EXT, 10);
 		}
 	}
 }
